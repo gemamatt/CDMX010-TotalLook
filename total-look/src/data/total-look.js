94cf2120f5e7175dcae2c29ecccdfb140bfc8b10
MichelleFragosoC <michellefragosoc@outlook.com>
2021-05-20 18:27:35 -0500

    Creación de dos categorías

--- total-look/src/data/total-look.js
+++ total-look/src/data/total-look.js
@@ -7,38 +7,155 @@ export default {
       precio: '$350.00 MXN',
       description: 'Esta prenda es ideal para la temporada de primavera, por su composición de algodón y 10% de spandex te ayudará a sentirte comoda, fresca y segura.',
       tallas: 'CH, M, G',
+      type: 'clothes'
     },
     {
       name: 'Vestido de Flores',
       color: 'Blanco',
       img: 'https://i.pinimg.com/originals/3f/94/19/3f94197ae5d2c0065538661a740afa95.jpg',
       precio: '$560.00 MXN',
-      description: 'L.',
+      description: 'Vestido confexionado en tela tipo nylon y estampado de flores.',
       tallas: 'CH, M, G, EG',
+      type: 'clothes'
+    },
+    { 
+      name: 'Vestido floral',
+      color: 'Rosa, Amarillo, Verde y Blanco',
+      img: 'https://img.ltwebstatic.com/images3_pi/2020/07/21/15953201599cf8478831f803600d9093500bcb8efd_thumbnail_600x.webp',
+      precio: '$380 MXN',
+      description: 'Vestido floral con fruncido de hombros descubiertos',
+      tallas: 'CH, M, G, EG',
+      type: 'clothes'
     },
     {
-      name: 'Blusa tipo campesina',
-      color: 'Blanca, Negra y Azul',
-      img: '',
-      precio: '$380.00 MXN',
-      description: 'L.',
+      name: 'Mono floreado',
+      color: 'Negro y blanco',
+      precio: '$190 MXN',
+      img:'https://img.ltwebstatic.com/images2_pi/2019/07/05/15623141281411924283_thumbnail_600x799.webp',
+      description: 'Mono de cintura con cordón de espalda con estampado floral',
       tallas: 'CH, M, G, EG',
+      type: 'clothes'
     },
     {
-      name: '',
-      color: '',
-      img: '',
-      precio: '$',
-      description: 'L.',
+      name: 'Pantalón bajo',
+      color:'Azul, verde y negro',
+      precio: '$225',
+      img: 'https://img.ltwebstatic.com/images3_pi/2019/09/26/1569488268caa678d07c1debaa268164b09e2cc5f9_thumbnail_600x.webp',
+      description: 'Pantalones bajos con abertura de cintura elástica',
       tallas: 'CH, M, G, EG',
+      type: 'clothes'
     },
     {
-      name: '',
-      color: '',
-      img: '',
-      precio: '$',
-      description: 'L.',
+      name: 'Top de tirantes',
+      color:'Blanco, azul, rosa, verde y amarillo',
+      precio: '$225 MXN',
+      img: 'https://img.ltwebstatic.com/images2_pi/2019/07/15/15631843564046627882_thumbnail_600x799.webp',
+      description: "Top de tirantes unicolor ribete en abanico",
       tallas: 'CH, M, G, EG',
+      type: 'clothes'
     },
+    {
+      name: 'Blusa campesina',
+      color:'Blanco, azul, rosa, verde y naranja',
+      precio: '$390 MXN',
+      img: 'https://i.pinimg.com/736x/b2/34/7e/b2347ee9e3f40febe6bba4e59e062fb9.jpg',
+      description: "Composición 100% de algodón ideal para la época de calor",
+      tallas: 'CH, M, G, EG',
+      type: 'clothes'
+    },
+    {
+      name: 'Short conb lazo',
+      color:'Blanco, azul, rosa, negro y rojo',
+      precio: '$680 MXN',
+      img: 'https://img.ltwebstatic.com/images2_pi/2019/06/19/15609303434234550774_thumbnail_600x799.webp',
+      description: "Shorts confexionados en denim, cuenta con una lazo en la cintura para ayudar a estilizar",
+      tallas: 'CH, M, G, EG',
+      type: 'clothes'
+    },
+    {
+      name: 'Sandalia sport multicolor',
+      img: 'https://static.pullandbear.net/2/photos//2021/V/1/1/p/1708/740/203/1708740203_2_2_8.jpg?t=1617887358932&imwidth=563',
+      description: 'Zapato tipo sandalia flatform multicolor. Tiras en el corte y cierre mediante tira adherente en el empeine y en la parte delantera. Suela tipo track. Altura de la suela 2.5 cm.',
+      price: '$480 MXN',
+      type: 'shoes'
+    },
+    {
+      name: 'Zueco serraje',
+      img: 'https://static.pullandbear.net/2/photos//2021/V/1/1/p/1428/740/131/1428740131_2_2_8.jpg?t=1620028150827&imwidth=563',
+      description: 'Zapato tipo zueco de piel en color arena. Corte de serraje con tira ajustable adhesiva en el lateral. Suela en combinación de materiales. Altura de la suela 2.5 cm.',
+      precio: '$590 MXN',
+      type: 'shoes'
+    },
+    {
+      name: 'Tenis abotinados puntera goma',
+      img: 'https://static.pullandbear.net/2/photos//2021/V/1/1/p/1202/740/001/1202740001_2_3_8.jpg?t=1611919958874&imwidth=563',
+      description: 'Zapato tipo tenis abotinado monocolor en crema. Cierre mediante agujetas. Puntera de goma. Suela gruesa combinada. Altura de la suela 4,5 cm.',
+      precio: '$675 MXN',
+      type: 'shoes'
+    },
+    {
+      name: 'Tenis casual suela volumen',
+      img: 'https://static.pullandbear.net/2/photos//2021/V/1/1/p/1202/740/001/1202740001_2_3_8.jpg?t=1611919958874&imwidth=563',
+      description: 'Zapato tipo tenis casual de color blanco con detalle de talonera en contraste color lila. Suela volumen y cierre mediante agujetas. Altura de la suela 3 cm.',
+      precio: '$745 MXN',
+      type: 'shoes'
+    },
+    {
+      name: 'Snoopy',
+      img: 'https://static.pullandbear.net/2/photos//2021/V/1/1/p/1218/640/009/1218640009_2_2_8.jpg?t=1597997381313&imwidth=563',
+      description: 'Zapato tipo tenis deportiva de Snoopy. En color azul con ilustración de Snoopy y lettering en la parte trasera. Cierre mediante agujetas con detalle de texto. Altura de la suela 3 cm.',
+      precio: '$799 MXN',
+      type: 'shoes'
+    },
+    {
+      name: 'Mocasín detalle hebilla',
+      img: 'https://static.pullandbear.net/2/photos//2021/V/1/1/p/1407/740/040/1407740040_2_3_8.jpg?t=1611847197498&imwidth=563',
+      description: 'Zapato tipo mocasín en color negro. Detalle de eslabones dorados en la parte delantera. Altura del tacón 2,5 cm.',
+      precio: '$578 MXN',
+      type: 'shoes'
+    },
+    {
+      name: 'Sandalia tacón negra',
+      img: 'https://static.pullandbear.net/2/photos//2021/V/1/1/p/1618/540/040/1618540040_2_2_8.jpg?t=1582027730003&imwidth=563',
+      description: 'Sandalia de mujer negra con tacón alto, cierre mediante hebilla en el tobillo y detalle de tira en la parte delantera. Disponible en otros colores. Altura del tacón: 9,5 cm. Altura del piso: 1,5 cm.',
+      precio: '$8,79MXN',
+      type: 'shoes'
+    },
+    {
+      name: 'Botín detalle cut out',
+      img: 'https://static.pullandbear.net/2/photos//2021/V/1/1/p/1106/640/040/1106640040_2_2_8.jpg?t=1598617109103&imwidth=563',
+      description: 'Botín de mujer en color negro, piel, con detalle cut out en los laterales y acabado en punta. Altura del tacón 8 cm.',
+      precio: '1,127 MXN',
+      type: 'shoes'
+    },
+    {
+      name: 'Sandalia negra',
+      img: 'https://static.pullandbear.net/2/photos//2021/V/1/1/p/1613/540/040/1613540040_2_2_8.jpg?t=1581338663082&imwidth=563',
+      description: 'Sandalia negra de mujer con tacón medio. Construcción con tira delantera y cierre mediante hebilla en pulsera al tobillo. Altura del tacón: 6 cm. Altura de la suela: 0,5 cm.',
+      precio: '$599 MXN',
+      type: 'shoes'
+    },
+    {
+      name: 'Cuña cruzada yute',
+      img: 'https://static.pullandbear.net/2/photos//2021/V/1/1/p/1401/840/111/1401840111_2_2_8.jpg?t=1617016409189&imwidth=563',
+      description: 'Tipo cuña de yute. Tiras cruzadas en la parte delantera y cierre mediante hebilla en pulsera al tobillo. Altura de la cuña 6.8 cm.',
+      precio: '$699 MXN',
+      type: 'shoes'
+    },
+    {
+      name: "Sandalia pala trenzada",
+      img: "https://static.pullandbear.net/2/photos//2021/V/1/1/p/1804/741/012/1804741012_2_2_8.jpg?t=1617783749101&imwidth=563",
+      description: "Zapato tipo sandalia plana de color azul. Tira acolchada en la parte delantera. Acabado en punta cuadrada. Altura de la suela 1 cm.",
+      precio: "$499 MXN",
+      type: "shoes"
+    },
+    {
+      name: "Bota track calcetín",
+      img:"https://static.pullandbear.net/2/photos//2021/V/1/1/p/1082/740/040/1082740040_2_3_8.jpg?t=1611765588608&imwidth=563",
+      description: "Zapato tipo botín de tacón de color negro. Corte en combinación de materiales. Laterales elásticos. Tirador trasero y delantero. Suela gruesa tipo track. Altura de la suela 6 cm.",
+      precio: "$1,299 MXN",
+      type: "shoes"
+    }
+    
   ],
 };
