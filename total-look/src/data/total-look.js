4c99a78a0aa53fcb693db9b78ed7a7bbe665338c
MichelleFragosoC <michellefragosoc@outlook.com>
2021-05-20 16:43:20 -0500

    Creación de la data

---
+++ total-look/src/data/total-look.js
@@ -0,0 +1,44 @@
+export default {
+  clothes: [
+    {
+      name: 'Conjunto de girasoles',
+      color: 'Negro, blanco y amarillo',
+      img: 'https://i.pinimg.com/originals/ca/c4/20/cac420d022d52730ae761ab1eba19f34.jpg',
+      precio: '$350.00 MXN',
+      description: 'Esta prenda es ideal para la temporada de primavera, por su composición de algodón y 10% de spandex te ayudará a sentirte comoda, fresca y segura.',
+      tallas: 'CH, M, G',
+    },
+    {
+      name: 'Vestido de Flores',
+      color: 'Blanco',
+      img: 'https://i.pinimg.com/originals/3f/94/19/3f94197ae5d2c0065538661a740afa95.jpg',
+      precio: '$560.00 MXN',
+      description: 'L.',
+      tallas: 'CH, M, G, EG',
+    },
+    {
+      name: 'Blusa tipo campesina',
+      color: 'Blanca, Negra y Azul',
+      img: '',
+      precio: '$380.00 MXN',
+      description: 'L.',
+      tallas: 'CH, M, G, EG',
+    },
+    {
+      name: '',
+      color: '',
+      img: '',
+      precio: '$',
+      description: 'L.',
+      tallas: 'CH, M, G, EG',
+    },
+    {
+      name: '',
+      color: '',
+      img: '',
+      precio: '$',
+      description: 'L.',
+      tallas: 'CH, M, G, EG',
+    },
+  ],
+};
