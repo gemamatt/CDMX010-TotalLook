e87d28a5cb44827b327c242c927b660f8d16bdcb
Karly-G <gonzalezcervantes.karla@gmail.com>
2021-05-20 20:17:52 -0500

    avance pagina principal

--- total-look/src/App.js
+++ total-look/src/App.js
@@ -1,15 +1,19 @@
 import './App.css';
 import Navbar from './Components/Navbar/Navbar'
+import Footer from './Components/Footer/Footer'
+import Principal from './Views/Principal/Principal';
 
 function App() {
   return (
     <div>
       <Navbar />
-    <div className="App">
-      <h1>Hello World</h1>
+      <div className="App">
+        <Principal/>
+      </div>
+      <footer>
+        <Footer/>
+      </footer>
     </div>
-    </div>
-    
   );
 }
 
--- total-look/src/Components/Navbar/Searcher/Searcher.js
+++ total-look/src/Components/Navbar/Searcher/Searcher.js
@@ -1,22 +1,40 @@
-import lupa from '../../../assets/lupa.png';
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import React, {useEffect, useState} from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {faSearch} from '@fortawesome/free-solid-svg-icons';
 import './Searcher.css';
 
 const Searcher =()=>{
+
+    // const [search,setSearch]= useState([]);
+    const initialStateValues = {
+        busqueda: '',
+    };
+
+    const [values, setValues] = useState(initialStateValues);
+
+    const handleInputChange = e => {
+        const {name, value} = e.target;
+        setValues({...values, [name]: value})
+        console.log(setValues.name);
+    };
+
+    // onChange=async e =>{
+    //     await this.setValues({busqueda: e.target.value});
+
+    // }
+
     return(
         <div className='searcher'>
-            {/* <img src={lupa} className="lupa" alt="lupa" /> */}
             <input
                     type="text"
                     placeholder="Buscar"
                     className="textField"
                     name="busqueda"
-                    // value={this.state.busqueda}
-                    // onChange={this.onChange}
+                    value={values.busqueda}
+                    onChange={handleInputChange}
                 />
-                <button type="button" className="btnBuscar" FontAwesomeIcon icon={faSearch} >
-
+                <button type="button" className="btnBuscar">
+                <FontAwesomeIcon icon={faSearch} />
                 </button>
         </div>
     )
