dbf21fda2f85b7710a0d316c87c5074c3c72dd27
MichelleFragosoC <michellefragosoc@outlook.com>
2021-05-20 23:56:06 -0500

    Esqueleto de pantalla

--- total-look/src/Promotions.js
+++ total-look/src/Promotions.js
@@ -1,3 +1,31 @@
 //LISTA DE PROMOCIONES
 //-Debe mostrar todas las promociones existentes sin paginación.
-//-Deberá de cumplir con el condicional para ocultar los botones de editar y eliminar promociones  y aparecerá el carrito de compras 
+//-Deberá de cumplir con el condicional para ocultar los botones 
+// de editar y eliminar promociones  y aparecerá el carrito de compras 
+
+import Rect from 'react'
+import Navbar from './Components/Navbar/Navbar'
+import Footer from './Components/Footer/Footer'
+import './Promotions.css';
+
+function Promotions(props) {
+    return (
+        return (
+            <div>
+                <Product
+                    key={p.id}
+                    name={p.name}
+                    picture={p.img}
+                    price={p.price}
+                    status={p.status}
+                    onSaveProduct={() => props.onSaveProduct(p.id)}
+                    onIncrementProduct={() => props.onIncrementProduct(p.id)}
+                    onRemoveProduct={() => props.onRemoveProduct(p.id)}
+                />
+            </div>
+        )
+        })}
+    
+}
+
+export default Promotions
