480cd194cd37e1e779e7b9caca0cc9ffc411af87
MichelleFragosoC <michellefragosoc@outlook.com>
2021-05-20 19:47:24 -0500

    Se agrego la categoría de bolsas

--- total-look/src/data/total-look.js
+++ total-look/src/data/total-look.js
@@ -4,7 +4,7 @@ export default {
       name: 'Conjunto de girasoles',
       color: 'Negro, blanco y amarillo',
       img: 'https://i.pinimg.com/originals/ca/c4/20/cac420d022d52730ae761ab1eba19f34.jpg',
-      precio: '$350.00 MXN',
+      price: '$350.00 MXN',
       description: 'Esta prenda es ideal para la temporada de primavera, por su composición de algodón y 10% de spandex te ayudará a sentirte comoda, fresca y segura.',
       tallas: 'CH, M, G',
       type: 'clothes'
@@ -13,7 +13,7 @@ export default {
       name: 'Vestido de Flores',
       color: 'Blanco',
       img: 'https://i.pinimg.com/originals/3f/94/19/3f94197ae5d2c0065538661a740afa95.jpg',
-      precio: '$560.00 MXN',
+      price: '$560.00 MXN',
       description: 'Vestido confexionado en tela tipo nylon y estampado de flores.',
       tallas: 'CH, M, G, EG',
       type: 'clothes'
@@ -22,7 +22,7 @@ export default {
       name: 'Vestido floral',
       color: 'Rosa, Amarillo, Verde y Blanco',
       img: 'https://img.ltwebstatic.com/images3_pi/2020/07/21/15953201599cf8478831f803600d9093500bcb8efd_thumbnail_600x.webp',
-      precio: '$380 MXN',
+      price: '$380 MXN',
       description: 'Vestido floral con fruncido de hombros descubiertos',
       tallas: 'CH, M, G, EG',
       type: 'clothes'
@@ -30,7 +30,7 @@ export default {
     {
       name: 'Mono floreado',
       color: 'Negro y blanco',
-      precio: '$190 MXN',
+      price: '$190.00 MXN',
       img:'https://img.ltwebstatic.com/images2_pi/2019/07/05/15623141281411924283_thumbnail_600x799.webp',
       description: 'Mono de cintura con cordón de espalda con estampado floral',
       tallas: 'CH, M, G, EG',
@@ -39,7 +39,7 @@ export default {
     {
       name: 'Pantalón bajo',
       color:'Azul, verde y negro',
-      precio: '$225',
+      price: '$225.00 MXN',
       img: 'https://img.ltwebstatic.com/images3_pi/2019/09/26/1569488268caa678d07c1debaa268164b09e2cc5f9_thumbnail_600x.webp',
       description: 'Pantalones bajos con abertura de cintura elástica',
       tallas: 'CH, M, G, EG',
@@ -48,7 +48,7 @@ export default {
     {
       name: 'Top de tirantes',
       color:'Blanco, azul, rosa, verde y amarillo',
-      precio: '$225 MXN',
+      price: '$225.00 MXN',
       img: 'https://img.ltwebstatic.com/images2_pi/2019/07/15/15631843564046627882_thumbnail_600x799.webp',
       description: "Top de tirantes unicolor ribete en abanico",
       tallas: 'CH, M, G, EG',
@@ -57,7 +57,7 @@ export default {
     {
       name: 'Blusa campesina',
       color:'Blanco, azul, rosa, verde y naranja',
-      precio: '$390 MXN',
+      price: '$390.00 MXN',
       img: 'https://i.pinimg.com/736x/b2/34/7e/b2347ee9e3f40febe6bba4e59e062fb9.jpg',
       description: "Composición 100% de algodón ideal para la época de calor",
       tallas: 'CH, M, G, EG',
@@ -66,7 +66,7 @@ export default {
     {
       name: 'Short conb lazo',
       color:'Blanco, azul, rosa, negro y rojo',
-      precio: '$680 MXN',
+      price: '$680.00 MXN',
       img: 'https://img.ltwebstatic.com/images2_pi/2019/06/19/15609303434234550774_thumbnail_600x799.webp',
       description: "Shorts confexionados en denim, cuenta con una lazo en la cintura para ayudar a estilizar",
       tallas: 'CH, M, G, EG',
@@ -83,79 +83,162 @@ export default {
       name: 'Zueco serraje',
       img: 'https://static.pullandbear.net/2/photos//2021/V/1/1/p/1428/740/131/1428740131_2_2_8.jpg?t=1620028150827&imwidth=563',
       description: 'Zapato tipo zueco de piel en color arena. Corte de serraje con tira ajustable adhesiva en el lateral. Suela en combinación de materiales. Altura de la suela 2.5 cm.',
-      precio: '$590 MXN',
+      price: '$590.00 MXN',
       type: 'shoes'
     },
     {
       name: 'Tenis abotinados puntera goma',
       img: 'https://static.pullandbear.net/2/photos//2021/V/1/1/p/1202/740/001/1202740001_2_3_8.jpg?t=1611919958874&imwidth=563',
       description: 'Zapato tipo tenis abotinado monocolor en crema. Cierre mediante agujetas. Puntera de goma. Suela gruesa combinada. Altura de la suela 4,5 cm.',
-      precio: '$675 MXN',
+      price: '$675 MXN',
       type: 'shoes'
     },
     {
       name: 'Tenis casual suela volumen',
       img: 'https://static.pullandbear.net/2/photos//2021/V/1/1/p/1202/740/001/1202740001_2_3_8.jpg?t=1611919958874&imwidth=563',
       description: 'Zapato tipo tenis casual de color blanco con detalle de talonera en contraste color lila. Suela volumen y cierre mediante agujetas. Altura de la suela 3 cm.',
-      precio: '$745 MXN',
+      price: '$745.00 MXN',
       type: 'shoes'
     },
     {
       name: 'Snoopy',
       img: 'https://static.pullandbear.net/2/photos//2021/V/1/1/p/1218/640/009/1218640009_2_2_8.jpg?t=1597997381313&imwidth=563',
       description: 'Zapato tipo tenis deportiva de Snoopy. En color azul con ilustración de Snoopy y lettering en la parte trasera. Cierre mediante agujetas con detalle de texto. Altura de la suela 3 cm.',
-      precio: '$799 MXN',
+      price: '$799.00 MXN',
       type: 'shoes'
     },
     {
       name: 'Mocasín detalle hebilla',
       img: 'https://static.pullandbear.net/2/photos//2021/V/1/1/p/1407/740/040/1407740040_2_3_8.jpg?t=1611847197498&imwidth=563',
       description: 'Zapato tipo mocasín en color negro. Detalle de eslabones dorados en la parte delantera. Altura del tacón 2,5 cm.',
-      precio: '$578 MXN',
+      price: '$578.00 MXN',
       type: 'shoes'
     },
     {
       name: 'Sandalia tacón negra',
       img: 'https://static.pullandbear.net/2/photos//2021/V/1/1/p/1618/540/040/1618540040_2_2_8.jpg?t=1582027730003&imwidth=563',
       description: 'Sandalia de mujer negra con tacón alto, cierre mediante hebilla en el tobillo y detalle de tira en la parte delantera. Disponible en otros colores. Altura del tacón: 9,5 cm. Altura del piso: 1,5 cm.',
-      precio: '$8,79MXN',
+      price: '$8,79MXN',
       type: 'shoes'
     },
     {
       name: 'Botín detalle cut out',
       img: 'https://static.pullandbear.net/2/photos//2021/V/1/1/p/1106/640/040/1106640040_2_2_8.jpg?t=1598617109103&imwidth=563',
       description: 'Botín de mujer en color negro, piel, con detalle cut out en los laterales y acabado en punta. Altura del tacón 8 cm.',
-      precio: '1,127 MXN',
+      price: '1,127.00 MXN',
       type: 'shoes'
     },
     {
       name: 'Sandalia negra',
       img: 'https://static.pullandbear.net/2/photos//2021/V/1/1/p/1613/540/040/1613540040_2_2_8.jpg?t=1581338663082&imwidth=563',
       description: 'Sandalia negra de mujer con tacón medio. Construcción con tira delantera y cierre mediante hebilla en pulsera al tobillo. Altura del tacón: 6 cm. Altura de la suela: 0,5 cm.',
-      precio: '$599 MXN',
+      price: '$599.00 MXN',
       type: 'shoes'
     },
     {
       name: 'Cuña cruzada yute',
       img: 'https://static.pullandbear.net/2/photos//2021/V/1/1/p/1401/840/111/1401840111_2_2_8.jpg?t=1617016409189&imwidth=563',
       description: 'Tipo cuña de yute. Tiras cruzadas en la parte delantera y cierre mediante hebilla en pulsera al tobillo. Altura de la cuña 6.8 cm.',
-      precio: '$699 MXN',
+      price: '$699 MXN',
       type: 'shoes'
     },
     {
       name: "Sandalia pala trenzada",
       img: "https://static.pullandbear.net/2/photos//2021/V/1/1/p/1804/741/012/1804741012_2_2_8.jpg?t=1617783749101&imwidth=563",
       description: "Zapato tipo sandalia plana de color azul. Tira acolchada en la parte delantera. Acabado en punta cuadrada. Altura de la suela 1 cm.",
-      precio: "$499 MXN",
+      price: "$499.00 MXN",
       type: "shoes"
     },
     {
       name: "Bota track calcetín",
       img:"https://static.pullandbear.net/2/photos//2021/V/1/1/p/1082/740/040/1082740040_2_3_8.jpg?t=1611765588608&imwidth=563",
       description: "Zapato tipo botín de tacón de color negro. Corte en combinación de materiales. Laterales elásticos. Tirador trasero y delantero. Suela gruesa tipo track. Altura de la suela 6 cm.",
-      precio: "$1,299 MXN",
+      price: "$1,299 MXN",
       type: "shoes"
+    },
+    {
+      name: 'Bolsa tipo crossbody papel trenzado solapa',     
+      img: 'https://static.pullandbear.net/2/photos//2021/V/1/1/p/4192/740/111/4192740111_2_2_8.jpg?t=1621414897057&imwidth=563',     
+      description: 'Bolsa tipo crossbody de color beige. Cuerpo de papel trenzado. Cierre mediante zipper y solapa con imán. Alto x Ancho x Fondo 16,5 x 23 x 6 cm.',      
+      price: '$549.00 MXN',      
+      type: 'bags'
+    },
+    {
+      name: 'Bolsa de mano papel trenzado dos tonos',      
+      img: 'https://static.pullandbear.net/2/photos//2021/V/1/1/p/4158/740/040/4158740040_2_1_8.jpg?t=1616689714049&imwidth=563',     
+      description: 'Bolsa de mano de color beige y negro. Papel trenzado en el corte. Detalle de cierre mediante nudo. Asas de mano y asa de hombro de color negro. Alto x Ancho x Fondo 14.5 x 18 x 11 cm.',      
+      price: '$549',     
+      type: 'bags'   
+    },
+    {
+      name: 'Bolsa shopper Mickey Mouse',      
+      img: 'https://static.pullandbear.net/2/photos//2021/V/1/1/p/4115/740/002/4115740002_2_1_8.jpg?t=1620647054858&imwidth=563',     
+      description: 'Bolsa formato shopper de tejido de color negro con ilustración de Mickey Mouse. Asas de hombro. Alto x Ancho x Fondo 42,5 x 45,5 x 1 cm.',      
+      price: '$239.00 MXN',      
+      type: 'bags'
+    },
+    {
+      name: 'Mini bolsa de mano nylon logo, Negro',     
+      img: 'https://static.pullandbear.net/2/photos//2021/V/1/1/p/4023/740/040/4023740040_2_1_8.jpg?t=1614774548529&imwidth=563',      
+      description: 'Bolsa de mano de nylon de color negro con bolsillo delantero. Con cierres. Asa de hombro con detalle de lettering. Detalle de asa extraíble de color lila con logo. Alto x Ancho x Fondo 11.5 x 16.5 x 8 cm.',
+      price: '$549.00 MXN',
+      type: 'bags'
+    },
+    {
+      name: 'Crossbody detalle cadenas',      
+      img: 'https://static.pullandbear.net/2/photos//2021/V/1/1/p/4079/640/040/4079640040_2_2_8.jpg?t=1606127807707&imwidth=563',
+      description: 'Bolsa tipo crossbody en color negro con detalle de cadena de asa corta y detalle de asa de hombro con cadena. Cierre mediante solapa e imán. Alto x Ancho x Fondo 15 x 23 x 5 cm.',      
+      price: '$299.00 MXN',     
+      type: 'bags'   
+    },
+    {
+      name: 'Mini shopper tejido',      
+      img: 'https://static.pullandbear.net/2/photos//2021/V/1/1/p/4054/640/040/4054640040_2_2_8.jpg?t=1617200732873&imwidth=563',      
+      description: 'Bolsa tipo mini shopper de tejido en color negro con detalle de bolsillo en la parte frontal. Cierre mediante imán y fruncido. Asas de mano y asa de hombro ajustable. Alto x Ancho x Fondo 22 x 32 x 16,5 cm.',      
+      price: '$399.00 MXN',      
+      type: 'bags'    
+    },
+    {
+      name: 'Crossbody estampado animal',
+      img: 'https://static.pullandbear.net/2/photos//2021/V/1/1/p/4127/640/201/4127640201_2_2_8.jpg?t=1606128141829&imwidth=563',
+      description: 'Bolsa tipo crossbody con estampado animal tipo serpiente con detalle de cadena de asa corta y detalle de asa de hombro con cadena. Cierre mediante solapa e imán. Alto x Ancho x Fondo 15 x 23 x 5 cm.',
+      price: '$299.00 MXN',
+      type: 'bags'   
+    },
+    {
+      name: 'Bolsa tipo crossbody papel trenzado',
+      img: 'https://static.pullandbear.net/2/photos//2021/V/1/1/p/4178/740/111/4178740111_2_1_8.jpg?t=1621414885513&imwidth=563',      
+      description: 'Bolsa tipo crossbody de color crema. Cuerpo de papel trenzado. Asa de hombro. Alto x Ancho x Fondo 37 x 27,5 x 16 cm.',
+      price: '$549.00 MXN',
+      type: 'bags'
+    },
+    {
+      name: 'Crossbody acolchada detalles metálicos',      
+      img: 'https://static.pullandbear.net/2/photos//2021/V/1/1/p/4110/640/102/4110640102_2_2_8.jpg?t=1616684662462&imwidth=563',      
+      description: 'Bolsa tipo crossbody disponible en varios colores. Tejido acolchado con detalle de pespuntes. Asa de hombro con detalle de cadena. Cierre mediante solapa con imán. Alto x Ancho x Fondo 15 x 23 x 6,5 cm.',      
+      price: '$399.00 MXN',      
+      type: 'bags'    
+    },
+    {
+      name: 'Mini shopper tejido',      
+      img: 'https://static.pullandbear.net/2/photos//2021/V/1/1/p/4182/740/111/4182740111_2_1_8.jpg?t=1619620632154&imwidth=563',      
+      description: 'Bolsa de mano mini de tejido disponible en varios colores. Detalle de bolsillo en la parte delantera. Cierre mediante pliegue. Asas de mano y asa de hombro ajustable y extraíble. Alto x Ancho x Fondo 19 x 22 x 9 cm.',      
+      price: '$549.00 MXN',      
+      type: 'bags'
+    },
+    {
+      name: 'Crossbody acolchado',    
+      img: 'https://static.pullandbear.net/2/photos//2021/V/1/1/p/4117/740/002/4117740002_2_2_8.jpg?t=1616690071565&imwidth=563',    
+      description: 'Bolsa de mano acolchada disponible en color negro o color crema. Cuerpo acolchado con detalle de pespuntes. Asa de hombro de cadena metalizada. Cierre mediante pieza metálica. Alto x Ancho x Fondo 14,5 x 20,5 x 8 cm.',
+      price: '$499.00 MXN',      
+      type: 'bags'    
+    },
+    {
+      name: 'Crossbody acolchada detalles metálicos',
+      img: 'https://static.pullandbear.net/2/photos//2021/V/1/1/p/4110/640/040/4110640040_2_2_8.jpg?t=1607675810699&imwidth=563',      
+      description: 'Bolsa tipo crossbody disponible en varios colores. Tejido acolchado con detalle de pespuntes. Asa de hombro con detalle de cadena. Cierre mediante solapa con imán. Alto x Ancho x Fondo 15 x 23 x 6,5 cm.',
+      price: '$399.00 MXN',      
+      type: 'bags'    
     }
-    
   ],
 };
